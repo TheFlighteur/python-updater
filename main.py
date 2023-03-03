97facc46227576a4f045c5e9181e5c15f89575e7
Pierre BRUN <flighteur@gmail.com>
2023-03-03 20:49:04 +0100

    Code cleanup and updater.py presence check

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-import requests
-import pathlib
+from requests import get
 from subprocess import Popen
 from time import sleep
+from os import remove, path
+
 i = 0
 version = 1
-currpath = pathlib.Path(__file__).parent.resolve()
+
 
 def check_version():
-    online_version = requests.get("https://raw.githubusercontent.com/TheFlighteur/python-updater/main/version")
-    return int(online_version.text)
+    return int(get("https://raw.githubusercontent.com/TheFlighteur/python-updater/main/version").text)
 
 
 def check_for_update():
     if check_version() > version:
         print("updating script")
         bootstrap_updater()
-        Popen("python {}/{}".format(currpath, "updater.py"))
+        Popen(f"python updater.py")
         quit()
 
 
 def bootstrap_updater():
-    f = open("./updater.py", "w")
+    f = open("updater.py", "w")
     f.write("""from requests import get
 from time import sleep
+from subprocess import Popen
 sleep(1)
-f = open("./main.py", "w")
+f = open("main.py", "w")
 f.write(get("https://raw.githubusercontent.com/TheFlighteur/python-updater/main/main.py").text)
 f.close()
+Popen("python main.py")
 """)
     f.close()
 
 
+if path.exists("updater.py"):
+    remove("updater.py")
 while i < 100:
     print(i)
     i += 1
