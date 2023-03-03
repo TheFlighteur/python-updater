5ddb9e139e6bd9b879ae0e7dec1ee90246ac262a
TheFlighteur <flighteur@gmail.com>
2023-03-03 22:04:32 +0100

    Updated to version 3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from requests import get
 from subprocess import Popen
 from time import sleep
-from os import remove, path
+from datetime import datetime
 
 i = 0
-version = 2
-# updated to version 2
+version = 3
+# updated to version 3
 
 
 def check_version():
@@ -15,30 +15,17 @@ def check_version():
 def check_for_update():
     if check_version() > version:
         print("updating script")
-        bootstrap_updater()
-        Popen(f"python updater.py")
+        f = open(__file__, "w")
+        f.write(get("https://raw.githubusercontent.com/TheFlighteur/python-updater/main/main.py").text)
+        f.close()
+        Popen(f"python main.py")
         quit()
 
-
-def bootstrap_updater():
-    f = open("updater.py", "w")
-    f.write("""from requests import get
-from time import sleep
-from subprocess import Popen
-sleep(1)
-f = open("main.py", "w")
-f.write(get("https://raw.githubusercontent.com/TheFlighteur/python-updater/main/main.py").text)
-f.close()
-Popen("python main.py")
-""")
-    f.close()
-
-
-if path.exists("updater.py"):
-    print("deleting updater")
-    remove("updater.py")
+        
+u = open("updated.txt", "a")
+u.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+u.close()
 while i < 100:
-    print(i)
     i += 1
     sleep(1)
     check_for_update()
